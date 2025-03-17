b5d171b4b47a3b253bc819fa7caa8b9524bbec88
Nils Programmierer <nils.programmierer@gmail.com>
2025-03-17 15:05:55 +0100

    first commit

--- script.js
+++ script.js
@@ -250,8 +250,9 @@ function activateDarkMode(layoutElements) {
     });
 }
 
+
 function initDatabase() {
-    let request = indexedDB.open("MyDatabase", 2);
+    let request = indexedDB.open("MyDatabase", 1);
 
     request.onerror = function (event) {
         console.error("Datenbank konnte nicht geöffnet werden: ", event.target.error);
